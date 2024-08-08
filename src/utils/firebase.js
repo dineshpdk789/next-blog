d50245371ef05da54b5540dfd3c139bab3d9af5b
Dinupdk <pdk7893@gmail.com>
2024-08-08 12:12:28 +0530

    Signed-off-by: Dinupdk <pdk7893@gmail.com>

---
+++ src/utils/firebase.js
@@ -0,0 +1,30 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE,
+  authDomain: "blog10-b154d.firebaseapp.com",
+  projectId: "blog10-b154d",
+  storageBucket: "blog10-b154d.appspot.com",
+  messagingSenderId: "656283821920",
+  appId: "1:656283821920:web:e885b1c8decc67744de005",
+  measurementId: "G-SVN752FC4K"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+
+
+
+
+
+
+
+
+/*
+mongodb+srv://dineshpdk789:Dineshpdk789@cluster0.oddf3gi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/
+
+*/
